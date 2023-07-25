e2fda71168c4d8c6c6abbbeacb727ec3455e7242
Padam Sethia <padamsethia5@gmail.com>
2023-07-25 14:57:05 +0530

    Fixes

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
 
         </div>
 
-        <div class="columns is-variable is-8 is-multiline">
+        <div class="columns mb-6 is-variable is-8 is-multiline">
             <div class="column is-4">
                 <p class="heading">
                     ABOUT US</p>
